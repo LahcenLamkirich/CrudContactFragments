e05e8f823eef8631594029289d5a1a9d79cc482c
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-04-14 16:15:58 +0000

    commit

--- app/src/main/java/com/example/crudcontact/profileFragment.java
+++ app/src/main/java/com/example/crudcontact/profileFragment.java
@@ -7,8 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class profileFragment extends Fragment {
+
+    Button btnAdd ;
+    EditText username, phoneNumber ;
+
 
     public profileFragment(){}
 
@@ -16,7 +22,13 @@ public class profileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view =  inflater.inflate(R.layout.fragment_profile, container, false);
-        
+
+        btnAdd = view.findViewById(R.id.btnAdd);
+        username = view.findViewById(R.id.username);
+        phoneNumber = view.findViewById(R.id.phoneNumber);
+
+
+
         return view ;
     }
 }
