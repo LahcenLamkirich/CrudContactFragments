668668c8e4ad4979a807f0942011fd217c669000
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-04-14 23:57:11 +0000

    commit

--- app/src/main/java/com/example/crudcontact/MainActivity.java
+++ app/src/main/java/com/example/crudcontact/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         // this two lines to adopt the view with the activity :
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
